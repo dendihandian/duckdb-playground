c9732ad5443114d4cf2810021096d99202b7694e
Dendi Handian <contact.dendi@yahoo.com>
2023-05-14 14:42:51 +0700

    csv export

---
+++ docker/duckdb-python/src/10-export-table-to-csv.py
@@ -0,0 +1,14 @@
+import duckdb
+
+# create a connection to a file called 'demo.duckdb'
+con = duckdb.connect('data/demo.duckdb')
+
+# query the table
+con.sql("COPY example_1 TO 'src/_example_1.csv' (HEADER, DELIMITER ',');")
+
+# query
+con.sql("SELECT * FROM 'src/_example_1.csv' LIMIT 50").show()
+
+"""
+docker-compose exec duckdb-python python src/10-export-table-to-csv.py
+"""
